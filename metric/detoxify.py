6867565d7a6ef2cd0495795362ee1f07ded6d099
141 <141>
2025-04-18 16:58:36 +0800

    delete unrelated files

--- metric/detoxify.py
+++
@@ -1,9 +0,0 @@
-from detoxify import Detoxify
-from typing import Dict, Optional, List
-
-class DetoxifyScorer:
-    def __init__(self,device="cuda:0"):
-        self.model = Detoxify('original', device=device)
-    def get_scores(self, input_text: str) -> Dict[str, float]:
-        return self.model.predict(input_text)
-
